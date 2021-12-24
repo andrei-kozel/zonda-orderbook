631401f1a7e6c000815ed9d5488bf99970795309
Andrei Kozel <kozel.andrei.94@gmail.com>
2021-12-24 12:00:34 +0100

    tailwindcss and cleanup

--- src/components/head/index.js
+++ src/components/head/index.js
@@ -1,15 +1,14 @@
 import React from "react";
-import "./style.css";
 
 export const Head = ({ currencyArray, title }) => {
   return (
-    <div className="head">
-      <div className="head__title">
+    <div className="flex flex-col font-bold">
+      <div className="flex justify-center text-lg py-1">
         <span colSpan="2">{title}</span>
       </div>
-      <div className="head__currency">
-        <span>Amount ({currencyArray[0]})</span>
-        <span>Price ({currencyArray[1]})</span>
+      <div className="flex place-content-around text-lg py-1">
+        <span className="py-1">Amount ({currencyArray[0]})</span>
+        <span className="py-1">Price ({currencyArray[1]})</span>
       </div>
     </div>
   );
--- src/components/row/index.js
+++ src/components/row/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import "./style.css";
 
 export const Row = ({ item, index }) => {
   return (
     <div
-      className="row"
+      className="w-full flex place-content-around py-1"
       style={{
         backgroundColor: (index + 1) % 2 === 1 ? "rgb(64 64 64)" : null,
       }}
     >
-      <div className="row__item"> {item.amount} </div>
-      <div className="row__item"> {item.rate} </div>
+      <div className="min-w-[100px]"> {item.amount} </div>
+      <div className="min-w-[100px]"> {item.rate} </div>
     </div>
   );
 };
